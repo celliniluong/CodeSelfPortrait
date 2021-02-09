444a685c0b1deb22ba20a8a8c07249b521a9100b
celliniluong <78052138+celliniluong@users.noreply.github.com>
2021-02-09 15:50:27 -0800

    Final changes
    Added last details, cleaned up stylistics.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,20 @@
-/********
+/************************
 Code Self Portrait Assignment
 By: Cellini Luong
 This assignment is focused on translating self portrait sketches into P5.js
-********/
+*************************/
 
-//Global
-var gDebugMode = true;
+//Global Variables
+var gDebugMode = false;
 var x = 299;
 var y = 299;
 
 // Setup code goes here
 function setup() {
   createCanvas(600,600);
-  noStroke();
  }
 
 
-// Background
 function draw() {
 	background(128, 128, 0);
 	
@@ -25,73 +23,105 @@ function draw() {
 		drawDebugInfo();
 	}
 
-	// Hair
+	// Draw Facial Features
+	drawHair();
+	drawNeck();
+	drawFace();
+	drawLips();
+	drawEyeball();
+	drawIris();
+	drawPupil();
+	drawBrows();
+	drawBangs();
+	drawNose();
+	drawBeautyMarks();
+	}
+
+function drawHair() {
 	fill(10);
 	noStroke();
 	rect(x - 120, y-40, 240, 200);
 	ellipse(x, y-40, 240, 300);
+}
 
-
-	// Face
+function drawNeck() {
+	noStroke();
 	fill(245, 222, 179);
-	ellipse(x, y - 60, 192, 230);
-
-	// Neck
 	quad(x - 30, 324, x + 30, 324, x + 50, 394, x - 50, 394);
 	ellipse(x, 396, 100, 60);
+}
 
-	// Lips
+function drawFace() {
+	fill(245, 222, 179);
+	stroke(222, 184, 135);
+	strokeWeight(2);
+	ellipse(x, y - 60, 182, 232);
+}	
+
+function drawLips() {
 	fill(240, 128, 128);
+	stroke(205, 92, 92);
+	strokeWeight(2);
 	ellipse(x + 6, y - 10, 24, 14);
 	ellipse(x - 6, y - 10, 24, 14);
+	noStroke();
 	ellipse(x , y - 6, 40, 18);
+	stroke(205, 92, 92);
+	line(281, 290, 317, 290);
+}
 
-	// Eyeshape
+function drawEyeball() {
+	noStroke();
 	fill(250, 250, 250);
 	ellipse(x - 40, y - 80, 40, 24);
 	ellipse(x + 40, y - 80, 40, 24);
-	
-	// Eyeball
+}
+
+function drawIris() {
 	fill(119, 75, 30);
 	ellipse(x - 40, y - 80, 24, 24);
 	ellipse(x + 40, y - 80, 24, 24);
+}
 
-	// Pupil
+function drawPupil() {
 	fill(0);
-	ellipse(x - 40, y - 76, 12, 12);
-	ellipse(x + 40, y - 76, 12, 12);
+	ellipse(x - 40, y - 80, 12, 12);
+	ellipse(x + 40, y - 80, 12, 12);
 	fill(20);
+}
 
-	// Brows
-	stroke(50);
+function drawBrows() {
+	stroke(30);
 	strokeWeight(4);
 	line(x + 18, y - 98, x + 48, y - 104);
 	line(x + 48, y - 104, x + 64, y - 98);
 	line(x - 18, y - 98, x - 48, y - 104);
 	line(x - 48, y - 104, x - 64, y - 98);
+}
 
-	// Bangs
+function drawBangs() {
 	fill(10);
 	noStroke();
 	triangle (x, y - 184, x - 100, y - 92, x - 70, y - 155);
 	triangle (x, y - 184, x + 100, y - 92, x + 70, y - 155);
+}
 
-	drawNose();
-	
-	}
-
-	
-
-function drawNose(){
+function drawNose() {
 	// Nose
 	stroke(222, 184, 135);
 	strokeWeight(3);
-	line(x, y - 60, x, y-80);
-	line(x, y - 61, x - 8, y - 48);
+	line(x, y - 52, x, y-86);
+	line(x, y - 53, x - 8, y - 40);
+	strokeWeight(4);
+	point(x, y - 40);
+	point(x + 6, y - 40);
+}
 
+function drawBeautyMarks() {
+	stroke(50);
+	strokeWeight(4);
+	point(310, 231);
 }	
-
-
 
 //Draw debug mode
 function drawDebugInfo() {
